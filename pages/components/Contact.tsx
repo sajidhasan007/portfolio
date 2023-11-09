72799126e0b1a3225f2533e33b0f4cecb403ad75
sajidhasan007 <sajid.hasan.cit@gmail.com>
2023-11-10 00:04:56 +0600

    add margin top on each section

--- pages/components/Contact.tsx
+++ pages/components/Contact.tsx
@@ -1,14 +1,14 @@
 import { HiOutlineMail } from "react-icons/hi";
 const Contact = () => {
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center mt-40">
       <div className="text-center">
-        <h1 className="text-[70px] font-black">Contact</h1>
-        <p className="text-base">
+        <h1 className="text-[70px] font-black text-center">Contact</h1>
+        <p className="text-base text-center">
           Shoot me an email if you want to connect! You can also find me on
           Linkedin or Twitter if that is more your speed.
         </p>
-        <div className="flex items-center gap-2">
+        <div className="flex justify-center items-center gap-2">
           <HiOutlineMail />
           <p className="font-bold text-xl">sajid.hasan.cit@gmail.com</p>
         </div>
